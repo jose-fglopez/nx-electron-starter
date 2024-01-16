dd720bcc77e70e2b2525b40639630b8e49eb162d
jose-fglopez <jose-fglopez@gmail.com>
2024-01-16 21:37:34 +0100

    server base files created

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -1,14 +1,3 @@
-import express from 'express';
+import { Server } from './server';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
-});
-
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+Server.startServer();
