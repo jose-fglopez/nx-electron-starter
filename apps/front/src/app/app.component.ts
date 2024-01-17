c1dcc55f06e33c1df9da0cf7850d20d4d018aedc
jose-fglopez <jose-fglopez@gmail.com>
2024-01-17 09:55:31 +0100

    add server testing call from front

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
@@ -11,4 +12,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 })
 export class AppComponent {
   title = 'front';
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(' http://localhost:3000').subscribe(
+      (res: unknown) => alert(JSON.stringify(res)),
+      (err) => alert(JSON.stringify(err))
+    );
+  }
 }
